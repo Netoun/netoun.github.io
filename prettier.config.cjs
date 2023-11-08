03dc7bd7ff73a45157c08ec6d840e35c00c1f4d9
nicolas <nicolas.coulonnier@ontexglobal.com>
2023-11-08 09:22:18 +0100

    optimize background image

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,6 +2,8 @@
 module.exports = {
     // i am just using the standard config, change if you need something else
     ...require('prettier-config-standard'),
+    singleQuote: false,
+    semi: false,
     pluginSearchDirs: [__dirname],
     plugins: [require.resolve('prettier-plugin-astro')],
     overrides: [
