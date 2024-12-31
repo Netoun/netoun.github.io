e61d5b4597fb3ca38f3c9d1ef43f16365af55c1e
nicolas <nicolas@lonestone.io>
2024-12-31 16:51:39 +0100

    fix redirect 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://netoun.com',
+  site: 'https://www.netoun.com',
   output: 'static',
   i18n: {
     defaultLocale: "en",
